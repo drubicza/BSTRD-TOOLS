67282960ac153a037bdaba6563860d0b5317a557
MRBSTRD <55237166+MRBSTRD@users.noreply.github.com>
2019-09-12 17:59:35 +0000

    Add files via upload

---
+++ BSTRD.sh
@@ -0,0 +1,235 @@
+#usr/bin/bash
+read -p "Jika tidak tau passwordnya tanyakan pada yang punya sc.
+WA : 083862170349
+Password: " pass
+if [ $pass = "naskleeng13" ]
+then
+clear
+toilet -f slant --gay "Success"
+sleep 0
+clear
+else
+  echo "\033[31;  salah njeer"
+  sleep 0
+  sh BSTRD.sh
+  fi
+clear
+figlet BSTRD TOOLS| lolcat
+echo "============================================================
+
+
+# Nama :Agung Prasetyo
+# WA   :083862170349
+#Github:https://github.com/MRBSTRD
+
+
+============================================================" | lolcat
+
+sleep 2
+
+echo "[0] INSTALL BAHAN DULU CUK
+[1] Dark V4
+[2] Xforce
+[3] Tools RUSMANA
+[4] SpamCALL
+[5] SpamSMS
+[6] SpamMASALL
+[7] DARK V3
+[8] KUMPULAN DARKFB V1
+[9] KUMPULAN DARKFB V2
+[10] KUMPULAN TOOLS TERMUX ID
+[11] BOT IGEH
+[12] DOWNLOAD MUSIK YT
+[13] DARK V1.6
+[14] DARK V1.7
+[15] DARK PREMIUM
+[16] DARK VIP
+[17] DARK V1.8
+[18] EXIT" | lolcat
+echo "[*]pilih nomor : " | lolcat
+read nomor
+
+if [ $nomor = 0 ] || [ $nomor = 0 ]
+then
+clear
+pkg update && pkg upgrade
+pkg install git python python2 bash nano figlet curl openssh ruby php nodejs unzip toilet mc
+gem install lolcat
+pip install mechanize requests bs4
+pip2 install mechanize requests bs4
+sh BSTRD.sh
+fi
+
+if [ $nomor = 1 ] || [ $nomor = 1 ]
+then
+clear
+pkg update && pkg upgrade
+pkg install git python2
+pip2 install mechanize requests
+git clone https://github.com/cyber2611/Dark4
+cd Dark4
+python2 Dark4.py
+fi
+
+if [ $nomor = 2 ] || [ $nomor = 2 ]
+then
+clear
+pkg update && pkg upgrade
+pkg install git python
+pip install mechanize requests
+git clone https://github.com/asmin19/xforce
+cd xforce
+python xforce.py
+fi
+
+if [ $nomor = 3 ] || [ $nomor = 3 ]
+then
+clear
+apt update
+apt upgrade
+pkg install toilet
+pkg install ruby
+pkg install gem
+pkg install php
+pkg install figlet
+gem install lolcat
+pkg install python2
+pip2 install mechanize
+pip2 install requests
+pkg install git
+git clone https://github.com/Rusmana-ID/rus
+cd rus
+sh v2.sh
+fi
+
+if [ $nomor = 4 ] || [ $nomor = 4]
+then
+clear
+pkg update && pkg upgrade
+pkg install git python
+pip install mechanize requests
+git clone https://github.com/404rgr/spamerCALL
+cd spamerCALL
+python spamercall.py
+fi
+
+if [ $nomor = 5 ] || [ $nomor = 5]
+then
+clear
+pkg update && pkg upgrade
+pkg install git python
+pkg install figlet
+gem install lolcat
+pip install mechanize requests
+git clone https://github.com/KANG-NEWBIE/SpamSms
+cd SpamSms
+python main.py
+fi
+
+if [ $nomor = 6 ] || [ $nomor = 6 ]
+then
+clear
+pkg install git python
+pip install mechanize requests
+git clone https://github.com/KANG-NEWBIE/C-SpamMasal
+cd C-SpamMasal
+python callmas.py
+fi
+
+if [ $nomor = 7 ] || [ $nomor = 7 ]
+then
+clear
+git clone https://github.com/cyber2611/dark3
+cd dark3
+python2 coding2.py
+fi
+
+if [ $nomor = 8 ] || [ $nomor = 8 ]
+then
+clear
+git clone https://github.com/HacketIndonedia1/Tools-darkv1
+cd Tools-darkv1
+sh tools-dark.sh
+fi
+
+if [ $nomor = 9 ] || [ $nomor = 9 ]
+then
+clear
+git clone https://github.com/fatonicyber/Darkcyber
+cd Darkcyber
+sh Darkcyber.sh
+fi
+
+if [ $nomor = 10 ] || [ $nomor = 10 ]
+then
+clear
+git clone https://github.com/perijoko14/TermuxID
+cd TermuxID
+python2 tools.py
+fi
+
+if [ $nomor = 11 ] || [ $nomor = 11 ]
+then
+clear
+git clone https://github.com/ikiganteng/bot-igeh
+cd bot-igeh
+unzip node_modules.zip
+node index
+fi
+
+if [ $nomor = 12 ] || [ $nomor = 12 ]
+then
+clear
+git clone https://github.com/BCA-X666X-MCC/YT
+cd YT
+sh setup.sh
+sh Smile.sh
+fi
+
+if [ $nomor = 13 ] || [ $nomor = 13]
+then
+clear
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+fi
+
+if [ $nomor = 14 ] || [ $nomor = 14]
+then
+clear
+git clone https://github.com/perijoko14/Dark-fb
+cd Dark-fb
+python2 dark.py
+fi
+
+if [ $nomor = 15 ] || [ $nomor = 15]
+then
+clear
+git clone https://github.com/THEmagizz/DarkPremium
+cd DarkPremium
+python2 darkFB.py
+fi
+
+if [ $nomor = 16 ] || [ $nomor = 16]
+then
+clear
+git clone https://github.com/yarrak68/darkpro
+cd darkpro
+python2 dark.py
+fi
+
+if [ $nomor = 17 ] || [ $nomor = 17]
+then
+clear
+git clone https://github.com/Mr-XsZ/Dark-fb
+cd Dark-fb
+python2 dark.py
+fi
+
+if [ $nomor = 18 ] || [ $nomor = 18]
+then
+clear
+echo "MAKASIH BROTHER
+SUDAH MAMPIR SEMOGA 
+ANDA TAMBAH CROT" | lolcat
+fi
